fbf6466b9e3ab8c892ac8d75c01ff76da7c85b59
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-12-10 14:54:02 -0300

    prepara revisão

--- src/components/FundoMobile.js
+++
@@ -1,9 +0,0 @@
-import IonIcons from "./IonIcons";
-
-
-export default function FundoMobile(){
-    return (<IonIcons seletorCss='fundo-mobile' icone={['home', 'search-outline','add-circle-outline', 
-                                                        'heart-outline', 'person-outline']} />
-    );
-    
-}
